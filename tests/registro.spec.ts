3becb786024c50badb359235306be7795fe16af9
Dario <dario.lopezsebastian@gmail.com>
2025-06-30 20:09:02 -0300

    aplicamos patron de estructura pom

--- tests/registro.spec.ts
+++ tests/registro.spec.ts
@@ -1,68 +1,75 @@
 import { test, expect } from '@playwright/test';
+import TestData from '../data/testData.json';
+import { RegisterPage } from '../pages/registerpage';
+import { BADFAMILY } from 'dns';
+
+
+
+let registerPage: RegisterPage;
+
+test.beforeEach(async ({ page }) => {
+  registerPage = new RegisterPage(page);
+  await registerPage.visitarPaginaRegistro();
+});
+
 
 test('TC-1 Verificación de elementos visuales en la péagina de registro', async ({ page }) => {
-  await page.goto('https://atena-redux.ngrok.app/signup');
-  await expect(page.locator('input[name=firstName]')).toBeVisible();
  // await expect(page.getByTestId('input-firstName-registro')).toBeVisible(); no se usa porque es un div y no un input
-  await expect(page.locator('input[name=lastName]')).toBeVisible();
-  await expect(page.locator('input[name=email]')).toBeVisible();
-  await expect(page.locator('input[name=password]')).toBeVisible();
-  await expect(page.getByTestId('boton-registrarse')).toBeVisible();
+ await expect(registerPage.firstNameInput).toBeVisible();
+  await expect(registerPage.lastNameInput).toBeVisible();
+  await expect(registerPage.emailInput).toBeVisible();
+  await expect(registerPage.passwordInput).toBeVisible();
+  await expect(registerPage.registerButton).toBeVisible();
   //await page.waitForTimeout(5000);
   // nos permite esperar 5 seg sin hacer nada)
 });
 
 test('TC-2 Verificar Boton de registro esta inhabilitado por defecto', async ({ page }) => {
   
-  await page.goto('https://atena-redux.ngrok.app/signup');
-  await expect(page.getByTestId('boton-registrarse')).toBeDisabled();
+  await expect(registerPage.registerButton).toBeDisabled();
 
 })
 
 
 test('TC-3 Verificar que el boton de registro se habilita al completar los campos', async ({ page }) => {
-  await page.goto('https://atena-redux.ngrok.app/signup');
-  await page.locator('input[name="firstName"]').fill('Dario')
-  await page.locator('input[name="lastName"]').fill('Lopez');
-  await page.locator('input[name="email"]').fill('dari@email.com');
-  await page.locator('input[name="password"]').fill('123456');
-  await expect(page.getByTestId('boton-registrarse')).toBeEnabled();
+
+    await registerPage.completarFormularioRegistro(TestData.usuarioValido);
+  //await registerPage.completarFormularioRegistro('Dario', 'Lopez','dari@gmail.com', '123456')
+  await expect (registerPage.registerButton).toBeEnabled();
 });
 
 
 
 test('TC-4 Verificar redireccionamiento a página de inicio de sesión al hacer clic en el botón de registro', async ({ page }) => {
-  await page.goto('https://atena-redux.ngrok.app/signup');
-  await page.getByTestId('boton-login-header-signup').click();
-  await expect(page).toHaveURL('https://atena-redux.ngrok.app/login');
+  await registerPage.loginButton.click();
+  await expect(page).toHaveURL('http://localhost:3000/login');
 });
 
 test('TC-5 Verificar Registro exitoso con datos válidos', async ({ page }) => {
-  await page.goto('https://atena-redux.ngrok.app/signup');
-  await page.locator('input[name="firstName"]').fill('Juan');
-  await page.locator('input[name="lastName"]').fill('Torres');
-  await page.locator('input[name="email"]').fill('juantorres'+Date.now().toString()+'@gmail.com');
-  await page.locator('input[name="password"]').fill('123456');
-  await page.getByTestId('boton-registrarse').click();
+  const email = (TestData.usuarioValido.email.split('@')[0]) + Date.now().toString() + '@' + TestData.usuarioValido.email.split('@')[1];
+  TestData.usuarioValido.email = email;
+  await registerPage.completarYHacerClickBotonRegistro(TestData.usuarioValido);
+  //await registerPage.completarYHacerClickBotonRegistro('Dario', 'Lopez','dari' +Date.now().toString()+'@gmail.com', '123456');
   await expect(page.getByText('Registro exitoso')).toBeVisible();
 });
 
 
 test('TC-6 Verificar que un usuario no pueda registrarse con un correo electrónico ya existente', async ({ page }) => {
-  const email = 'juantorres' + Date.now().toString() + '@email.com';
-  await page.goto('https://atena-redux.ngrok.app/signup');
-  await page.locator('input[name="firstName"]').fill('Juan');
-  await page.locator('input[name="lastName"]').fill('Torres');
-  await page.locator('input[name="email"]').fill(email);
-  await page.locator('input[name="password"]').fill('123456');
-  await page.getByTestId('boton-registrarse').click();
+  const email = (TestData.usuarioValido.email.split('@')[0]) + Date.now().toString() + '@' + TestData.usuarioValido.email.split('@')[1];
+  // forma sin el json de data test const email = 'juantorres' + Date.now().toString() + '@email.com';
+  TestData.usuarioValido.email = email;
+  await registerPage.completarFormularioRegistro(TestData.usuarioValido);
+  await registerPage.hacerClickBotonRegistro();
   await expect(page.getByText('Registro exitoso')).toBeVisible();
-  await page.goto('https://atena-redux.ngrok.app/signup');
-  await page.locator('input[name="firstName"]').fill('Juan');
-  await page.locator('input[name="lastName"]').fill('Torres');
-  await page.locator('input[name="email"]').fill(email);
-  await page.locator('input[name="password"]').fill('123456');
-  await page.getByTestId('boton-registrarse').click();
+  await page.goto('http://localhost:3000/');
+  await registerPage.completarFormularioRegistro(TestData.usuarioValido);
+  await registerPage.hacerClickBotonRegistro();
   await expect(page.getByText('Email already in use')).toBeVisible();
   await expect(page.getByText('Registro exitoso')).not.toBeVisible();
 });
+
+/*test('TC-7 Verificar inicio de sesión exitoso con credenciales válidas', async ({ page }) => {
+  await loginPage.completarYHacerClickBotonLogin(TestData.usuarioValido);
+  await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
+  await expect(dashboardPage.dashboardTitle).toBeVisible();
+});*/       
